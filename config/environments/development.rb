dc88669395ef4e2e5cf5bb245143261abcaf38d2
Brice Roehrig <broehrig.contact@gmail.com>
2022-03-29 14:24:45 +0200

    cloudinary set up

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,8 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
@@ -32,7 +34,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :cloudinary
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
