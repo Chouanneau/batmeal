64e0675049c7404692d11bc207b0633187e7611c
Brice Roehrig <broehrig.contact@gmail.com>
2022-03-29 16:18:47 +0200

    geo unfinished

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :orders
   has_many :reviews
 
+  geocoded_by :address_street_number, :address_street, :city, :zipcode, :country
+  after_validation :geocode, if: :will_save_change_to_address?
 end
