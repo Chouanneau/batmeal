e27b48b1d8ee6fe0c40ac8b03c1c2842408c4b64
Brice Roehrig <broehrig.contact@gmail.com>
2022-03-30 13:13:25 +0200

    ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_29_135349) do
+ActiveRecord::Schema.define(version: 2022_03_30_111300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 2022_03_29_135349) do
     t.datetime "date_time_end"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_meals_on_user_id"
   end
 
   create_table "ordered_meals", force: :cascade do |t|
@@ -111,6 +113,7 @@ ActiveRecord::Schema.define(version: 2022_03_29_135349) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "meals", "users"
   add_foreign_key "ordered_meals", "meals"
   add_foreign_key "ordered_meals", "orders"
   add_foreign_key "orders", "users"
