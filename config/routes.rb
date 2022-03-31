296ae656511f57fafbb2a79c04fbfdb11259f1ba
David Simoes <dvdsms@gmail.com>
2022-03-31 16:21:06 +0200

    orders controller created + routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :meals
+  resources :orders
 
   get "/my_meals", to: "meals#my_meals"
 end
