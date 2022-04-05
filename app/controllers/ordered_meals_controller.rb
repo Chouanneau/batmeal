1cd49f4aa656e14364e0e77106b5a94bb77d82d5
David Simoes <dvdsms@gmail.com>
2022-04-05 08:35:45 +0200

    some updates

--- app/controllers/ordered_meals_controller.rb
+++ app/controllers/ordered_meals_controller.rb
@@ -1,13 +1,21 @@
 class OrderedMealsController < ApplicationController
+  def new
+    @ordered_meal = Ordered_meal.new
+  end
+
   def create
+    raise
     @ordered_meal = Ordered_meal.new(ordered_meal_params)
     @ordered_meal.order = @order
+    raise
     @ordered_meal.save
+
+    redirect_to meals_path
   end
 
   private
 
-  def set_ordered_meal
-    @ordered_meal = Ordered_meal.find(params[:id])
+  def meal_params
+    params.require(:ordered_meal).permit(:quantity)
   end
 end
