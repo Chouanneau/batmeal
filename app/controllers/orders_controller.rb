94d3f35ea4631e2cbcfa40ac827f9e00ac516e8d
Matthieu Chouanneau <m.chouanneau@gmail.com>
2022-04-06 15:45:31 +0200

    confirmation et paiement

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
     @ordered_meals = @order.ordered_meals
+    @total_price = @order.total_price
   end
 
   def confirmation
